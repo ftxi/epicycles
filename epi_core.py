d2e48f44301e8d5a2f8c666b36be3498ad84bf29
ss <1307689960@qq.com>
2017-04-02 22:08:12 +0800

    better gif

--- epi_core.py
+++ epi_core.py
@@ -10,10 +10,12 @@ This is a saparated package.
 import numpy as np
 from math import sin, cos
 import imageio
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw #, ImageFilter
 
 
-def gif(filename, size, speed, r, p, n, l, line_min=5) :
+def gif(filename, size, speed, r, p, n, l, line_min=5, filter_zero=True) :
+    DIFF = 2
+    size *= DIFF
     N = len(n)
     ts = np.linspace(0, 2*np.pi, 200, endpoint=False)
     imgbase = Image.new('RGB', (2*size, 2*size), (255, 255, 255))
@@ -26,9 +28,11 @@ def gif(filename, size, speed, r, p, n, l, line_min=5) :
         _x, _y = 0.0, 0.0
         
         for k in range(N) :
-            _x += r[k] * cos(l[k]*t*n[k] + p[k])
-            _y += r[k] * sin(l[k]*t*n[k] + p[k])
-            draw.ellipse((int(x-r[k])+size, int(y-r[k])+size, int(x+r[k])+size, int(y+r[k])+size), outline=(128, 128, 128))
+            if filter_zero and n[k] == 0:
+                continue
+            _x += r[k] * DIFF * cos(l[k]*t*n[k] + p[k])
+            _y += r[k] * DIFF * sin(l[k]*t*n[k] + p[k])
+            draw.ellipse((int(x-r[k]*DIFF)+size, int(y-r[k]*DIFF)+size, int(x+r[k]*DIFF)+size, int(y+r[k]*DIFF)+size), outline=(128, 128, 128))
             if r[k] > line_min :
                 draw.line((int(x)+size, int(y)+size, int(_x)+size, int(_y)+size), fill=(0, 128, 128))
             x, y = _x, _y
@@ -36,7 +40,7 @@ def gif(filename, size, speed, r, p, n, l, line_min=5) :
             drawbase.line((_x+size, _y+size, create_image._x_+size, create_image._y_+size), (64, 64, 256))
         create_image.first = False
         create_image._x_, create_image._y_ = _x, _y
-        return np.array(img)
+        return np.array(img.resize((int(size/DIFF*2), int(size/DIFF*2)), Image.ANTIALIAS))
     
     create_image._x_ = 0.0
     create_image._y_ = 0.0
